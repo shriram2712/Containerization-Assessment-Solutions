50a6561dea3dae60dea4d32cdadce1b1b4d3a369
Shriram Rajaraman <32219070+shriram2712@users.noreply.github.com>
2019-07-31 18:40:36 +0530

    q3 app.py updated

--- q3/app/app.py
+++ q3/app/app.py
@@ -1,12 +1,13 @@
 import time
-
+#Import the required modules
 import redis
 from flask import Flask
 
+#Initialize the modules
 app = Flask(__name__)
 cache = redis.Redis(host='redis', port=6379)
 
-
+#Function to get redis hit count
 def get_hit_count():
     retries = 5
     while True:
@@ -18,7 +19,7 @@ def get_hit_count():
             retries -= 1
             time.sleep(0.5)
 
-
+#Routing '/'
 @app.route('/')
 def hello():
     count = get_hit_count()
